c4483df6263134e1bc8416c1cced9063af461e1d
toby <tobypatten@gmail.com>
2020-02-26 22:47:13 +0000

    map icon n stuff

---
+++ galartmap/MapsActivity.java
@@ -0,0 +1,59 @@
+package com.example.galartmap;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Environment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        Drawable myDrawable = getResources().getDrawable(R.drawable.icon1);
+        Bitmap icon = ((BitmapDrawable) myDrawable).getBitmap();
+        // Add a marker in Sydney and move the camera
+        LatLng bath = new LatLng(51.3801212, -2.3595492);
+        mMap.addMarker(new MarkerOptions().position(bath).title("Centre of camera").icon(BitmapDescriptorFactory.fromBitmap(icon)));
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bath,15f));
+    }
+}
