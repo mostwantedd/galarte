5da542c08fdd056a6f1f0c94f6a5d9564d1e1b15
mostwantedd <41048992+mostwantedd@users.noreply.github.com>
2020-03-09 12:37:35 +0000

    Add files via upload

---
+++ main/java/com/example/gallerypage/GalleryPage.java
@@ -0,0 +1,91 @@
+package com.example.gallerypage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class GalleryPage extends AppCompatActivity implements OnMapReadyCallback {
+
+    LinearLayout viewEventNames;
+    Button eventButton1,eventButton2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery_page);
+
+
+        viewEventNames = (LinearLayout) findViewById(R.id.viewEventNames);
+        buildCategoryScroll();
+        addListenerOnButtons();
+
+        //TODO: map link -> make button send you to the map page
+        //TODO: solve event -> drop down?
+        //TODO: add picture title city price  -> link to data base
+        //TODO: add scroll bar
+        //TODO: back button
+        //TODO
+
+
+
+    }
+
+
+    protected void addListenerOnButtons() {
+        eventButton1 = (Button) findViewById(R.id.btn1);
+        eventButton2 = (Button) findViewById(R.id.btn2);
+        Button mapButton = (Button)findViewById(R.id.button_to_map_activity);
+
+        eventButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                startActivity(new Intent(GalleryPage.this, MapActivity.class));   ///TODO: make button send you to the map page
+//                Intent Event1 = new Intent(view.getContext(),Event1.class);
+//                startActivityForResult(Event1,0);
+            }
+        });
+
+//        eventButton2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent questionnaire = new Intent(view.getContext(), QuestionPage.class);
+//                startActivityForResult(questionnaire,0);
+//            }
+//        });
+    }
+
+
+    private void buildCategoryScroll() {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, 10, 30, 10);
+
+        for (int i = 1; i <= 15; i++) {
+            final Button btnEvent = new Button(GalleryPage.this);
+            btnEvent.setText(String.valueOf(i));
+            btnEvent.setTextSize(16f);
+//            btnEvent.setAllCaps(false);
+//            btnEvent.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent));
+//            btnEvent.setTextColor(ContextCompat.getColor(this, android.R.color.black));
+            btnEvent.setLayoutParams(layoutParams);
+            btnEvent.setTag(i);
+            viewEventNames.addView(btnEvent);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
