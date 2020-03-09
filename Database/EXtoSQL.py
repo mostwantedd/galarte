4bf50603b64b9ffc05b0e79440de2fcce42465d4
bertrand <t.bertrand_arkos@icloud.com>
2020-03-09 09:17:07 +0000

    Excel To DB working as intended - pyhton module

--- Database/EXtoSQL.py
+++ Database/EXtoSQL.py
@@ -2,14 +2,15 @@ import xlrd
 import MySQLdb
 
 # Open the workbook and define the worksheet
-book = xlrd.open_workbook("test DB.xlsx")
+book = xlrd.open_workbook("Database/test DB.xlsx", encoding_override ='utf-8')
 sheet_names = book.sheet_names()
+# Establish a MySQL connection
+database = MySQLdb.connect (host="localhost", user = "root", passwd = "ip20", db = "ip")
+print("connected to the DB")
+
 for i in range (0,len(sheet_names)):
 	sheet = book.sheet_by_name(sheet_names[i])
-	# Establish a MySQL connection
-	database = MySQLdb.connect (host="localhost", user = "root", passwd = "ip20", db = "ipproject")
-	print("connected to the DB")
-
+	
 	# Get the cursor, which is used to traverse the database, line by line
 	cursor = database.cursor()
 
@@ -20,36 +21,69 @@ for i in range (0,len(sheet_names)):
 		query = """INSERT INTO AUTHOR (name, style, born,death,description) VALUES (%s, %s, %s,%s,%s)"""
 	elif sheet_names[i] == "ART_PIECE":
 		query = """INSERT INTO ART_PIECE (name, author,style,date, description) VALUES (%s, %s, %s,%s,%s)"""
-	else :
-		query = """INSERT INTO PLACE (name, email,country,city) VALUES (%s, %s, %s,%s)"""
+	elif sheet_names[i] == "PLACE":
+		query = """INSERT INTO PLACE (name, price,country,city) VALUES (%s, %s, %s,%s)"""
 
 	# Create a For loop to iterate through each row in the XLS file, starting at row 2 to skip the headers
 	for r in range(1, sheet.nrows):
 		if sheet.cell(r,0).value != "":   #check if the first cell is empty
-			name		    = sheet.cell(r,0).value
+			name = sheet.cell(r,0).value
 			if sheet_names[i] == "STYLE":
+				cursor.execute("""SELECT name FROM STYLE WHERE name = '%s' """ % (name.strip()))
+				check =  cursor.fetchall()
+				if(name == check[0][0]):
+					break
+				
 				era	            = int(sheet.cell(r,1).value)
 				description	    = sheet.cell(r,2).value
-
+				
 				values = (name, era, description)
 
 			elif sheet_names[i] == "AUTHOR":
+				cursor.execute("""SELECT name FROM AUTHOR WHERE name = '%s' """ % (name.strip()))
+				check =  cursor.fetchall()
+				if(name == check[0][0]):
+					break
+
 				style 			= sheet.cell(r,1).value
-				born 			= sheet.cell(r,2).value
-				death 			= sheet.cell(r,3).value
+				cursor.execute("""SELECT ID FROM STYLE WHERE name = '%s' """ % (style.strip()))
+				style_id =  cursor.fetchall()
+				
+
+				born 			= int(sheet.cell(r,2).value)
+				death 			= int(sheet.cell(r,3).value)
 				description 	= sheet.cell(r,4).value
 
-				values = (name, style,born,death, description)
+				values = (name, style_id[0],born,death, description)
 
 			elif sheet_names[i] == "ART_PIECE":
 				author 			= sheet.cell(r,1).value
+				cursor.execute("SELECT ID FROM AUTHOR WHERE name='%s' """ % (author.strip()))
+				author_id =  cursor.fetchall()
+
 				style 			= sheet.cell(r,2).value
-				date 			= sheet.cell(r,3).value
+				cursor.execute("SELECT ID FROM STYLE WHERE name='%s' """ % (style.strip()))
+				style_id =  cursor.fetchall()
+
+				date 			= int(sheet.cell(r,3).value)
 				description 	= sheet.cell(r,4).value
 
-				values = (name, author,style,date, description)			
+				values = (name, author_id[0],style_id[0],date, description)			
+
+			elif sheet_names[i] == "PLACE":
+				cursor.execute("""SELECT name FROM PLACE WHERE name = '%s' """ % (name.strip()))
+				check =  cursor.fetchall()
+				if(name == check[0][0]):
+					break
+
+				price 			= int(sheet.cell(r,1).value)
+				country 		= sheet.cell(r,2).value
+				city 			= sheet.cell(r,3).value
+				
+				values = (name,price,country,city)	
 
 			# Execute sql Query
+			print(values)
 			cursor.execute(query, values)
 
 	# Close the cursor
