29640d7d8a4116ed5160a8426a19ce7eb8a9719f
mostwantedd <41048992+mostwantedd@users.noreply.github.com>
2020-03-26 16:47:58 +0000

    Update MapsActivity.java

--- app/src/main/java/com/activities/galarte/MapsActivity.java
+++ app/src/main/java/com/activities/galarte/MapsActivity.java
@@ -26,6 +26,9 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MapsActivity extends AppCompatActivity
         implements
@@ -36,6 +39,13 @@ public class MapsActivity extends AppCompatActivity
     private GoogleMap mMap;
 
     private boolean mPermissionDenied = false;
+
+
+    double[] longitude = {-2.358266, -2.358713, 2.33759, -2.358359,
+            -2.350833, -2.359225, -2.365695, -2.363773, -2.3611, -2.360547};
+    double[] latitude = {51.381164, 51.383048, 48.860971, 51.382807,
+            51.385896, 51.38096, 51.386764, 51.381143, 51.382938, 51.383358};
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -50,6 +60,7 @@ public class MapsActivity extends AppCompatActivity
         }
     }
 
+//TODO: work on having the map show galleries on it using info from the db
 
 
 
@@ -107,6 +118,13 @@ public class MapsActivity extends AppCompatActivity
 
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bath,15f));
+
+        //show icons of galleries
+        for(int count = 0; count<longitude.length; count++){
+            LatLng coord = new LatLng(latitude[count], longitude[count]);
+            mMap.addMarker(new MarkerOptions().position(coord).title("Gallery Title").snippet("Details").icon(BitmapDescriptorFactory.fromBitmap(icon)));
+
+        }
     }
     private void enableMyLocation() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
